4ff252cda195595ca40d20e5078e8f17946ba423
JohanWikstrom <johan.wikstrom@lund.ntig.se>
2018-01-15 11:53:31 +0100

    started with dictonary for frequency

--- Tree/Tree/Form1.cs
+++ Tree/Tree/Form1.cs
@@ -14,12 +14,23 @@ namespace Tree
     public partial class Form1 : Form
     {
         private string hobbit;
-        
+        private int antal,antal1;
         public Form1()
         {
             InitializeComponent();
             hobbit = File.ReadAllText(System.IO.Directory.GetCurrentDirectory()+"/lord_of_the_rings-chapter1.txt");
-            label1.Text = hobbit;
+            label3.Text = hobbit;
+
+            foreach (char mellan in hobbit)
+            {
+                if(mellan == ' ')
+                antal++;
+
+                antal1++;
+            }
+            label1.Text = antal.ToString();
+            label2.Text = antal1.ToString();
+
         }
 
         private void label1_Click(object sender, EventArgs e)
