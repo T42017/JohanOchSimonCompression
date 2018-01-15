e640350085f46ae025f34043ab0328ba2eaa46d1
JohanWikstrom <johan.wikstrom@lund.ntig.se>
2018-01-15 11:26:13 +0100

    Added project

---
+++ Tree/Tree/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tree
+{
+    public partial class Form1 : Form
+    {
+        private string hobbit;
+        
+        public Form1()
+        {
+            hobbit = File.ReadAllText(@"C:\Users\usr\Downloads\lord_of_the_rings-chapter1.txt");
+          
+            InitializeComponent();
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
