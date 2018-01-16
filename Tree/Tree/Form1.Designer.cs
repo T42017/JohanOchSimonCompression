a8f6026a5ff4781b73afb185200f3eba41fbf148
JohanWikstrom <johan.wikstrom@lund.ntig.se>
2018-01-16 13:09:02 +0100

    fixes

--- Tree/Tree/Form1.Designer.cs
+++ Tree/Tree/Form1.Designer.cs
@@ -26,6 +26,7 @@ namespace Tree
             this.button3 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -80,11 +81,22 @@ namespace Tree
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(658, 302);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(227, 72);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "´load an encoded file";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1920, 1200);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -107,6 +119,7 @@ namespace Tree
         private Button button3;
         private OpenFileDialog openFileDialog1;
         private SaveFileDialog saveFileDialog1;
+        private Button button4;
     }
 }
 
--- Tree/Tree/Form1.cs
+++ Tree/Tree/Form1.cs
@@ -13,19 +13,13 @@ namespace Tree
 {
     public partial class Form1 : Form
     {
-        private string hobbit,b;
+        private string hobbit;
         private Dictionary<char, int> Frequencydictionary;
         public Form1()
         {
             InitializeComponent();
             Frequencydictionary = new Dictionary<char, int>();
-            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;                     
-           
-            //foreach (KeyValuePair<char, int> varen in Frequencydictionary)
-            //{
-            //    b = b + "\n" + String.Format("{0}, {1}", varen.Key, varen.Value);
-            //}
-            //label3.Text = b;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;                                           
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -54,8 +48,6 @@ namespace Tree
             saveFileDialog1.ShowDialog();
         }   
 
-
-
         private void button3_Click(object sender, EventArgs e)
         {
             textBox1.Clear();
@@ -68,6 +60,12 @@ namespace Tree
             pictureBox1.ImageLocation = System.IO.Directory.GetCurrentDirectory() + "/gang.jpg";
         }
 
-       
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+              
+            }
+        }
     }
 }
