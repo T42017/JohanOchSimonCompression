afe0bf125eaad741792dae67b62f4af14ef08b71
JohanWikstrom <johan.wikstrom@lund.ntig.se>
2018-01-16 09:50:01 +0100

    changed a bit in the code and added a nice background picture

--- Tree/Tree/Form1.Designer.cs
+++ Tree/Tree/Form1.Designer.cs
@@ -21,50 +21,70 @@ namespace Tree
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-
+            // 
+            // label1
+            // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 66);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(65, 81);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
-            
-
+            // 
+            // label2
+            // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(49, 100);
+            this.label2.Location = new System.Drawing.Point(65, 123);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(46, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
-
+            // 
+            // label3
+            // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(49, 144);
+            this.label3.Location = new System.Drawing.Point(65, 161);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(46, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "label3";
-            this.label3.ForeColor = Color.HotPink;
-            this.label3.BackColor = Color.Aquamarine;
-
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(893, 579);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(566, 469);
+            this.ClientSize = new System.Drawing.Size(890, 574);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
         #endregion
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Tree/Tree/Form1.cs
+++ Tree/Tree/Form1.cs
@@ -13,14 +13,14 @@ namespace Tree
 {
     public partial class Form1 : Form
     {
-        private string hobbit;
-        private Dictionary<char, int> Frequencydictionary = new Dictionary<char, int>();
-        private int antal;
+        private string hobbit,b;
+        private Dictionary<char, int> Frequencydictionary;
         public Form1()
         {
             InitializeComponent();
-            hobbit = File.ReadAllText(System.IO.Directory.GetCurrentDirectory()+"/lord_of_the_rings-chapter1.txt");
-            label3.Text = hobbit;
+            Frequencydictionary = new Dictionary<char, int>();
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            hobbit = File.ReadAllText(System.IO.Directory.GetCurrentDirectory() + "/lord_of_the_rings-chapter1.txt");
             foreach (char karak in hobbit)
             { 
                 if (Frequencydictionary.ContainsKey(karak))
@@ -31,11 +31,19 @@ namespace Tree
                 {
                    Frequencydictionary.Add(karak,1);
                 }                
-            }            
+            }
+            foreach (KeyValuePair<char, int> varen in Frequencydictionary)
+            {
+                b=b+"\n"+String.Format("{0}, {1}",varen.Key, varen.Value);
+            }           
+            label3.Text = b;
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-
+           
+            pictureBox1.ImageLocation = Directory.GetCurrentDirectory() + "/gang.png";           
         }
+
+       
     }
 }
